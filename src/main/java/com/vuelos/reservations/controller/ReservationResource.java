f654a32ab6a98596e1eceb0c4e11f0049e4b0645
RobertoSD22 <roberto.shirasago@gmail.com>
2025-03-23 15:57:28 -0600

    OPEN API (Documentación)
    - Segunda versión de la documentación de los endpoints de reserva
    - Se agregaron las posibles excepciones
    - Se agregaron ejemplos de lo que se envia en el body

--- src/main/java/com/vuelos/reservations/controller/ReservationResource.java
+++ src/main/java/com/vuelos/reservations/controller/ReservationResource.java
@@ -1,10 +1,12 @@
 package com.vuelos.reservations.controller;
 
+import com.vuelos.reservations.dto.ErrorDTO;
 import com.vuelos.reservations.dto.ReservationDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -31,6 +33,14 @@ public interface ReservationResource {
                             mediaType = MediaType.APPLICATION_JSON_VALUE,
                             schema = @Schema(implementation = List.class)
                     )
+                ),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Algo malo ocurrió al momento de obtener las reservas",
+                        content = @Content(
+                                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                schema = @Schema(implementation = ErrorDTO.class)
+                        )
                 )
             })
     public ResponseEntity<List<ReservationDTO>> getReservations();
@@ -52,7 +62,9 @@ public interface ReservationResource {
                 @Parameter(
                         in = ParameterIn.PATH,
                         name = "id",
-                        description = "Identificador de la reserva"
+                        description = "Identificador de la reserva",
+                        required = true,
+                        example = "1"
                 )
             })
     public ResponseEntity<ReservationDTO> getReservationById(@Min(1) @PathVariable Long id);
@@ -69,7 +81,47 @@ public interface ReservationResource {
                                     schema = @Schema(implementation = ReservationDTO.class)
                             )
                     )
-            })
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Información de la reserva a crear",
+                    required = true,
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ReservationDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Reserva",
+                                    summary = "Ejemplo de un registro para reserva",
+                                    value = "{\n" +
+                                            "    \"passengers\": [\n" +
+                                            "        {\n" +
+                                            "            \"firstName\": \"Andres\",\n" +
+                                            "            \"lastName\": \"Sacco\",\n" +
+                                            "            \"documentType\": \"DNI\",\n" +
+                                            "            \"documentNumber\": \"12345678\",\n" +
+                                            "            \"birthday\": \"1985-01-01\"\n" +
+                                            "        }\n" +
+                                            "    ],\n" +
+                                            "    \"itinerary\": {\n" +
+                                            "        \"segments\": [\n" +
+                                            "            {\n" +
+                                            "                \"origin\": \"MEX\",\n" +
+                                            "                \"destination\": \"BUE\",\n" +
+                                            "                \"departure\": \"2024-12-31\",\n" +
+                                            "                \"arrival\": \"2025-01-01\",\n" +
+                                            "                \"carrier\": \"AA\"\n" +
+                                            "            }\n" +
+                                            "        ],\n" +
+                                            "        \"price\": {\n" +
+                                            "            \"totalPrice\": 1,\n" +
+                                            "            \"basePrice\": 1,\n" +
+                                            "            \"totalTax\": 0\n" +
+                                            "        }\n" +
+                                            "    }\n" +
+                                            "}"
+                            )
+                    )
+            )
+    )
     public ResponseEntity<ReservationDTO> createReservation(@Valid @RequestBody ReservationDTO reservationDTO);
 
     @Operation(
@@ -89,14 +141,57 @@ public interface ReservationResource {
                     @Parameter(
                             in = ParameterIn.PATH,
                             name = "id",
-                            description = "Identificador de la reserva a actualizar"
+                            description = "Identificador de la reserva a actualizar",
+                            required = true,
+                            example = "1"
                     ),
                     @Parameter(
                             in = ParameterIn.DEFAULT,
                             name = "reservationDTO",
-                            description = "Información de la reserva a actualizar"
+                            description = "Información de la reserva a actualizar",
+                            required = true
                     )
-            })
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Información de la reserva a crear",
+                    required = true,
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ReservationDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Reserva",
+                                    summary = "Ejemplo de un registro para reserva",
+                                    value = "{\n" +
+                                            "    \"id\": 1,\n" +
+                                            "    \"passengers\": [\n" +
+                                            "        {\n" +
+                                            "            \"firstName\": \"Andres\",\n" +
+                                            "            \"lastName\": \"Sacco\",\n" +
+                                            "            \"documentType\": \"DNI\",\n" +
+                                            "            \"documentNumber\": \"12345678\",\n" +
+                                            "            \"birthday\": \"1985-01-01\"\n" +
+                                            "        }\n" +
+                                            "    ],\n" +
+                                            "    \"itinerary\": {\n" +
+                                            "        \"segments\": [\n" +
+                                            "            {\n" +
+                                            "                \"origin\": \"MEX\",\n" +
+                                            "                \"destination\": \"BOG\",\n" +
+                                            "                \"departure\": \"2024-12-31\",\n" +
+                                            "                \"arrival\": \"2025-01-01\",\n" +
+                                            "                \"carrier\": \"AA\"\n" +
+                                            "            }\n" +
+                                            "        ],\n" +
+                                            "        \"price\": {\n" +
+                                            "            \"totalPrice\": 1,\n" +
+                                            "            \"basePrice\": 1,\n" +
+                                            "            \"totalTax\": 0\n" +
+                                            "        }\n" +
+                                            "    }\n" +
+                                            "}"
+                            )
+                    )
+            ))
     public ResponseEntity<ReservationDTO> updateReservation(
             @Min(1) @PathVariable Long id, @Valid @RequestBody ReservationDTO reservationDTO);
 
@@ -111,13 +206,31 @@ public interface ReservationResource {
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                                     schema = @Schema(implementation = Void.class)
                             )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No encuentra la reserva a eliminar o no existe",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Algo malo ocurrió al momento de eliminar la reserva",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorDTO.class)
+                            )
                     )
             },
             parameters = {
                     @Parameter(
                             in = ParameterIn.PATH,
                             name = "id",
-                            description = "Identificador de la reserva a eliminar"
+                            description = "Identificador de la reserva a eliminar",
+                            required = true,
+                            example = "1"
                     )
             })
     public ResponseEntity<Void> deleteReservation(@Min(1) @PathVariable Long id);
